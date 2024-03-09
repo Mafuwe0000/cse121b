49e297e6d9d977fc848bcb31656d83f7bb99355e
Mafuwe0000 <mafuwemanuel@gmail.com>
2024-03-09 23:34:04 +0200

    week two assignment done

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,8 +6,8 @@
 
 
 let fullName = "Mafuwe Manuel";
-const currentYear = "2024";
-let profilePicture='images/mfw.jpg';
+const currentYear = '2024';
+let profilePicture='images/placeholder.jpg';
 
 
 
@@ -15,34 +15,32 @@ let profilePicture='images/mfw.jpg';
 
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
-
-const yearElement = document.querySelector("#year");
-/// the # in the selector string to indicate the id attribute value. 🪲This syntax detail is often missed.
-
-const imageElement = document.getElementById("profilePicture")
+const yearElement = document.querySelector("year");
+let imageElement=document.querySelector("img");
 
 /* Step 4 - Adding Content */
 
     ///Assign the nameElement's innerHTML property the fullName variable value
 nameElement.innerHTML=`<strong>${fullName}</strong>`;
 
-Element.textContent = "currentYear";
-
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', profileImageOf [fullName])
+imageElement.setAttribute('alt', `'profileImageOf ${fullName}'`)
 
 /* Step 5 - Array */
-const favoriteFoods = ["Arroz","Batata-frita","Salada","Torradas","Massa"]
+const favoriteFoods = ["Arroz","Batata-frita","Salada","Torradas","Massa"];
 
-document.getElementById(foodElement).innerHTML = {favoriteFoods}
+foodElement.innerHTML = `${favoriteFoods}`;
 
+///foodElement = favoriteFoods;
 
+const  oneFavoriteFood = ["ervilhas"];
 
+favoriteFoods.push("ervilhas");
 
 
 foodElement.innerHTML += `<br>${favoriteFoods}`
 
-favoriteFoods.slice(0,1)
-
-
-
+favoriteFoods.shift()
+foodElement.innerHTML += `<br>${favoriteFoods}`
+favoriteFoods.pop()
+foodElement.innerHTML += `<br>${favoriteFoods}`
