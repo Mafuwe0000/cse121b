cc9a72d5cbeac6a009e801ed085e236912d6e1fd
Mafuwe0000 <mafuwemanuel@gmail.com>
2024-04-07 01:22:19 +0200

    project half done

--- scripts/project.js
+++ scripts/project.js
@@ -1,4 +1,80 @@
-const body= document.body
-const headingOne = document.createElement('h1');
-body.append(h1);
-headingOne.textContent = "Veja "
+
+
+
+const addcountry = document.getElementById('add-country');
+const removecountry = document.getElementById('remove-country');
+const refresh = document.getElementById('filter-country');
+
+// Calling the function 
+getCountry();
+getCountry();
+getCountry();
+getCountry();
+
+
+
+
+let data =[];
+
+// fetch random book from api
+async function getCountry(){
+    const url = "https://restcountries.com/v3.1/all";
+
+
+    const response = await fetch(url);
+    const dados = await response.json();
+    //console.log(dados);
+    
+    //Retrieving data from JSON
+    function randomCountry(dados) {
+        return dados[Math.floor(Math.random() * dados.length)];
+    }
+    //console.log(randomCountry(dados));
+    //console.log(paises); 
+    let resultado = randomCountry(dados)  ;
+
+
+    const country = {
+        countryName :`${'Continent:   '}${resultado.continents} \n ${"Country:  "} ${resultado.altSpellings[1]} ${'Capital:  '}  ${resultado.capital[0]} ${"TIME:  "} ${resultado.timezones
+    [0]        }`
+    }
+     console.log(country);
+
+    addData(country);
+
+    } 
+//add new object to data array
+function addData (obj){
+    data.push(obj);
+
+    updateDOM()
+}
+
+//update DOM
+function updateDOM(providedData= data){
+    //clear main div
+    main.innerHTML = `<h2> <strong> </strong> </h2>`;
+
+    providedData.forEach(item =>{
+        const element = document.createElement('div');
+        element.classList.add('country');
+        element.innerHTML= `<strong>${item.countryName}</strong>`;
+        main.appendChild(element);
+    })
+}
+
+
+function reloadLocation(){
+    location.reload();
+}
+
+
+// event Listener
+
+addcountry.addEventListener('click', getCountry);
+refresh.addEventListener("click", reloadLocation)
+//filtercountry.addEventListener('click', filterCountry())
+    
+
+    
+
